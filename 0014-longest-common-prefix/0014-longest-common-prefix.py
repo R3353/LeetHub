b21ab97e5fc54a872c7be9902302411ab70f1745
Reese Mendoza <94931229+R3353@users.noreply.github.com>
2025-07-14 04:57:33 -0700

    Time: 3 ms (35.86%), Space: 12.5 MB (26.41%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,3 +1,14 @@
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        answer = ''
+        for i in range(len(strs[0])):
+            for word in strs:
+                if i == len(word) or strs[0][i] != word[i]:
+                    return answer
+            answer += strs[0][i]
+        return answer    
         
